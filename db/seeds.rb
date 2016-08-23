064eaaca0b17b98ce33d9952af50545f23abf136
Isabella Ong <isabellaong@gmail.com>
2016-08-23 19:27:31 +0800

    removes seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Role.destroy_all
-Section.destroy_all
-Project.destroy_all
+# Role.destroy_all
+# Section.destroy_all
+# Project.destroy_all
