2c711155cd8cb1e62d10f534e2b03ceaea1aa67c
Isabella Ong <isabellaong@gmail.com>
2016-08-23 18:20:01 +0800

    updates seed to destroy projects, sections and roles data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Project.destroy_all
+Section.destroy_all
+Role.destroy_all
